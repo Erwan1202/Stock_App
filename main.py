bcc39f4ec5fffac40e74994bbf1999b78b09aa45
Lord Erwan <erwan.a.marechal@gmail.com>
2024-12-04 12:27:13 +0100

    Refactor stock prediction app to load data directly from Yahoo Finance and remove unused code

--- main.py
+++ main.py
@@ -1,46 +1,16 @@
-import streamlit as st
 from datetime import date 
 
 import yfinance as yf
 from prophet import prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+import pandas as pd
 
-START = "2015-01-01"
+# Load data
+data = yf.download("AAPL", start="2020-01-01", end=date.today().strftime("%Y-%m-%d"))
 
-TODAY = date.today().strftime("%Y-%m-%d")
+# Fit model
+model = prophet.Prophet()
+model.fit(data)
 
-st.title("Stock Prediction App")
-
-stocks = ("AAPL", "GOOG", "MSFT", "GME", "AMC", "TSLA")
-selected_stock = st.selectbox("Select dataset for prediction", stocks)
-
-n_years = st.slider("Years of prediction:", 1, 4)
-period = n_years * 365
-
-@st.cache
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    return data
-
-data_load_state = st.text("Load data...")
-
-data = load_data(selected_stock)
-
-data_load_state.text("Loading data...done!")
-
-st.subheader('Raw data')
-
-st.write(data.tail())
-
-# Plot raw data
-def plot_raw_data():
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
-    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
-
-plot_raw_data()
 
